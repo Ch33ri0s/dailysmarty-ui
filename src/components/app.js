debf092add0fed4c5b7b9b77c5f4336456bf6fe5
Tenyson Partridge <Tenyson@Mirandas-MacBook-Pro.local>
2018-08-23 15:48:03 -0600

    created recent posts and search bar components

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Logo from "./logo";
+import SearchBar from "./searchBar";
+import RecentPosts from './recentPosts';
 
 export default class App extends Component {
   render() {
     return (
-      <div className='logo-main'>
-        <img src="https://s3-us-west-2.amazonaws.com/images-devcamp/React+Development/React+Project+Five%3A+Build+an+API+Search+Engine/Setting+Up+A+Logo+Component+%23+1793/image11.png"/>
+      <div>
+        <Logo />
+        <SearchBar />
+        <RecentPosts />
       </div>
     );
   }
--- src/components/logo.js
+++ src/components/logo.js
@@ -7,8 +7,8 @@ class Logo extends Component {
 
     render() {
         return (
-            <div>
-                
+            <div className="logo-main">
+                <img src="https://s3-us-west-2.amazonaws.com/images-devcamp/React+Development/React+Project+Five%3A+Build+an+API+Search+Engine/Setting+Up+A+Logo+Component+%23+1793/image11.png"/>
             </div>
         );
     }
