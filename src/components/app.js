e63f8b0c127a26fd8c8b8a39a85ed54006a6be86
Tenyson Partridge <Tenyson@Mirandas-MacBook-Pro.local>
2018-08-23 15:09:17 -0600

    setup log component and corresponding styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Logo from "./logo";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-      Logo
+      <div className='logo-main'>
+        <img src="https://s3-us-west-2.amazonaws.com/images-devcamp/React+Development/React+Project+Five%3A+Build+an+API+Search+Engine/Setting+Up+A+Logo+Component+%23+1793/image11.png"/>
       </div>
     );
   }
---
+++ src/components/logo.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Logo extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Logo;
